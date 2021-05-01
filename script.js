585693fd205680aa0431619aa0f898f6b7783fb2
Helena <helenasleterkesto@gmail.com>
2021-05-01 18:16:32 +0200

    Add functionality to capitalize account name and display to user on page

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 "use strict";
 
+// Seleting elements
+const accountBtn = document.querySelector(".accountBtn");
+const balance = document.querySelector(".balance");
+const withdraw = document.querySelector(".withdraw");
+const deposit = document.querySelector(".deposit");
+const exit = document.querySelector(".exit");
+const enter = document.querySelector(".enter");
+const input = document.querySelector(".input");
+const output = document.querySelector(".output");
+
 const account = {
   accountName: "john doe",
   balance: 100,
@@ -11,10 +21,29 @@ const account = {
   exitAccount: function () {},
 };
 
+// Capitalize account holder name
+const capitalizeAccountName = function (name) {
+  const names = name.split(" ");
+  const namesUpper = [];
+
+  for (const n of names) {
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  return namesUpper;
+};
+
+// Get account details and display on page
+const userData = accountBtn.addEventListener("click", function () {
+  document.querySelector(
+    ".output"
+  ).textContent = `Account Holder: ${capitalizeAccountName(
+    account.accountName
+  )}`;
+});
+
 function atm() {
-  let choice = parseInt(
-    prompt("Please select a transaction: BALANCE, DEPOSIT or WITHDRAWAL")
-  );
+  let choice = parseInt();
+  //prompt("Please select a transaction: BALANCE, DEPOSIT or WITHDRAWAL")
   //prompt user for choice.
 
   // you can use the variable choice for your switch or if/else statement
@@ -22,7 +51,7 @@ function atm() {
 }
 
 // In the deposit and withdrawal function use this code to show the prompt
-parseFloat(prompt("Put some proper message here"));
+//parseFloat(prompt("Put some proper message here"));
 // You need to change the message of course. You should also do some research why parseFloat() is used here
 // answer by making a comment in the code.
 // Be aware of that you need to add some code to this so you can display the actual input.
@@ -41,10 +70,10 @@ alert("your code here and maybe properties");
 // of course you need to put a proper message and you might need to display some of the properties.
 
 // STARTER FUNCTION
-function atm() {
-  let choice = parseInt(prompt("Display menu choices here"));
-  //prompt user for choice.
+// function atm() {
+//   let choice = parseInt(prompt("Display menu choices here"));
+//prompt user for choice.
 
-  // you can use the variable choice for your switch or if/else statement
-  // so choice will hold the value of the user input.
-}
+// you can use the variable choice for your switch or if/else statement
+// so choice will hold the value of the user input.
+// }
